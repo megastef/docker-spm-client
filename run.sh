d666b148035b02bc937982a0d73dc5e8bf723585
megastef <s.thies@bigdata-analyst.de>
2015-09-20 12:15:17 +0200

    changed logging and fix for multiple config entries

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-set -e
 # Configure SPM 
+#Set the field separator to new line
+IFS_ORIGINAL=$IFS
+IFS=";"
 if [ -n "$SPM_CFG" ]; then
-	for cfg in "$(echo $SPM_CFG | tr ";" "\n")"
+	for cfg in $SPM_CFG
 	do
-	  echo "$cfg"
-	  bash /opt/spm/bin/spm-client-setup-conf.sh ${cfg}
+	  echo "-" "$cfg"  
+	  bash /opt/spm/bin/spm-client-setup-conf.sh $cfg
 	  # Check for standalone monitors
 	  case "$cfg" in
 	    *standalone* ) export SPM_STANDALONE="enabled"; echo "Standalone Monitor enabled for: $cfg";;
@@ -13,13 +15,15 @@ if [ -n "$SPM_CFG" ]; then
 	done
 fi
 
+IFS=$IFS_ORIGINAL
+
 /etc/init.d/collectd start
 /etc/init.d/spm-sender start
 if [  "$SPM_STANDALONE_MONITOR" == "disabled" ]; then
 	echo "Standalone monitor is disabled"
 else
 	/etc/init.d/spm-monitor start
-	tail -f /opt/spm/spm-monitor/logs/spm-monitor.log &
+	tail -F /opt/spm/spm-monitor/logs/spm-monitor.log | grep -v INFO &
 fi
-tail -f /opt/spm/spm-sender/logs/spm-sender.log
+tail -F /opt/spm/spm-sender/logs/spm-sender.log | grep -v INFO
 
